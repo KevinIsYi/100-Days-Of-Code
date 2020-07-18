048e887c6426f4a7b7dec7a05bfa42ff3e5730e2
KevinIsYi <rodriguezkevin95@hotmail.com>
2020-07-18 11:11:27 -0500

    I added sorting macros and lcd/alldivisords functions

--- CP/TEMPLATE.cpp
+++ CP/TEMPLATE.cpp
@@ -2,9 +2,36 @@
 
 using namespace std;
 
+#define all(x) begin(x), end(x)
+#define allr(x) rbegin(x), rend(x)
+
 template <typename T>
 T GCD(T a, T b) {
 	return b ? GCD(b, a % b) : a;
+}
+
+long long LCD(long long a, long long b) {
+	if (a < b) {
+		swap(a, b);
+	}
+	
+	a /= GCD(a, b);
+	return a * b;
+}
+
+template <typename T>
+vector<int> allDivisors(T n) {
+	vector<T> divisors;
+	
+	for (int i = 1 ; i <= sqrt(n) ; ++i) {
+		if (n % i == 0) {
+			divisors.push_back(i);
+			if (n / i != i) {
+				divisors.push_back(n / i);
+			}
+		}
+	}
+	return divisors;
 }
 
 int main() {
