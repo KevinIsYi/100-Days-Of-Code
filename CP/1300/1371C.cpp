c96b392f93b89ca8b792e356f8fa5d23a7a0d128
KevinIsYi <rodriguezkevin95@hotmail.com>
2020-07-27 13:11:19 -0500

    solution of problem 1371C

---
+++ CP/1300/1371C.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+
+template <typename T>
+T GCD(T a, T b) {
+	return b ? GCD(b, a % b) : a;
+}
+
+long long LCD(long long a, long long b) {
+	if (a < b) {
+		swap(a, b);
+	}
+	a /= GCD(a, b);
+	return a * b;
+}
+
+template <typename T>
+vector<T> allDivisors(T n) {
+	vector<T> divisors;
+	
+	for (int i = 1 ; i <= sqrt(n) ; ++i) {
+		if (n % i == 0) {
+			divisors.push_back(i);
+			if (n / i != i) {
+				divisors.push_back(n / i);
+			}
+		}
+	}
+	return divisors;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	long long a, b, n, m;
+	
+	cin >> t;
+	while(t--) {
+		cin >> a >> b >> n >> m;
+		
+		if (a > b) {
+			swap(a, b);
+		}
+		if (a < m || a + b < n + m) {
+			cout << "No\n";
+		}
+		else {
+			cout << "Yes\n";
+		}
+	}
+	
+	return 0;
+}
