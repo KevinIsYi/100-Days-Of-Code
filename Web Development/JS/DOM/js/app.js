8cda8dde10c498a576932059ab68f476ffb7f243
KevinIsYi <rodriguezkevin95@hotmail.com>
2020-08-02 13:02:37 -0500

    Add and remove elements with js

--- Web Development/JS/DOM/js/app.js
+++ Web Development/JS/DOM/js/app.js
@@ -23,6 +23,61 @@ console.log(barra.children[0].children[0].children);
 
 
 // Traveling from child to parent
+/*
 let enlace = document.querySelectorAll(".enlace");
 console.log(enlace[0].parentNode);
 console.log(enlace[4].previousElementSibling);
+*/
+// Create js components
+
+/*
+const enlace = document.createElement('a');
+enlace.className = "enlace";
+enlace.id = "nuevo-id";
+enlace.setAttribute("href", "#");
+enlace.textContent = "Nuevo Enlace";
+
+/*add it to html 
+document.querySelector('#secundaria').appendChild(enlace);
+console.log(enlace);
+*/
+
+/*
+// edit current html with js
+const newHeading = document.createElement('h2');
+newHeading.id = "newHeader";
+newHeading.appendChild(document.createTextNode('New Text'));
+const prev = document.querySelector("#encabezado");
+const parent = document.querySelector("#lista-cursos");
+
+// reemplazar
+parent.replaceChild(newHeading, prev);
+*/
+
+/*
+//add and remove elements
+const enlaces = document.querySelectorAll(".enlace");
+const nav = document.querySelector("#principal");
+
+//enlaces[0].remove();
+nav.removeChild(enlaces[0]);
+
+console.log(enlaces);
+*/
+
+//create class
+/*
+const primerLi = document.querySelector(".enlace");
+let element;
+
+element = primerLi.classList.add("nueva-clase");
+element = primerLi.classList.remove("nueva-clase");
+element = primerLi.className;
+element = primerLi.classList;
+
+element = primerLi.getAttribute('href');
+primerLi.setAttribute("href", "http://google.com.mx");
+primerLi.removeAttribute("href");
+console.log(primerLi);
+console.log(element);
+*/
