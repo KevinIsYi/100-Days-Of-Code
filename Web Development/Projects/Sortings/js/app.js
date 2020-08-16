e34cf46b1d81345d8cd4ee31a39d3b09359ea2f3
KevinIsYi <rodriguezkevin95@hotmail.com>
2020-08-15 20:11:03 -0500

    Added a visual representation of insertion and selection sort

--- Web Development/Projects/Sortings/js/app.js
+++ Web Development/Projects/Sortings/js/app.js
@@ -41,12 +41,66 @@ function bubbleSort(arr) {
     }
 }
 
+function selectionSort(arr) {
+
+    let min;
+
+    for (let i = 0 ; i < arr.length ; ++i) {
+        setTimeout(() => {
+            min = i;
+            for (let j = i + 1 ; j < arr.length ; ++j) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if (i != min) {
+                let first = document.querySelector(`#idn${i}`),
+                    second = document.querySelector(`#idn${min}`),
+                    aux = arr[i];
+
+                first.style.height = `${arr[min]}px`;
+                second.style.height = `${arr[i]}px`;
+
+                arr[i] = arr[min];
+                arr[min] = aux;
+            }   
+        }, 10 * i);   
+    }
+}
+
+function insertionSort(arr) {
+
+    let aux, curr, first, second, j;
+
+    for (let i = 1 ; i < arr.length ; ++i) {
+        setTimeout(() => {
+            aux = arr[i];
+
+            for (j = i ; j > 0 && aux < arr[j - 1] ; --j) {
+                first = document.querySelector(`#idn${j}`);
+                second = document.querySelector(`#idn${j - 1}`);
+
+                first.style.height = `${arr[j - 1]}px`;
+                arr[j] = arr[j - 1];
+            }
+
+            if (i != j) {
+                curr = document.querySelector(`#idn${j}`);
+                curr.style.height = `${aux}px`;
+                arr[j] = aux;
+            }
+        }, 10 * i);
+        
+    }
+}
+
 function main() {
     let arr = [];
     fillArray(arr);
     createBars(arr);
     bubbleSort(arr);
-    
+    //selectionSort(arr);
+    //insertionSort(arr);
 }
 
 main();
