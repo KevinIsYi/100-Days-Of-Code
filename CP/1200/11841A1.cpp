0faddfb13cd499b8e1ea3f3f635fb7842a4d4b19
KevinIsYi <rodriguezkevin95@hotmail.com>
2020-08-17 14:21:29 -0500

    Solutions of problems 115B, 1174B, 1184A1, 1185B

---
+++ CP/1200/11841A1.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y);
+
+template <typename T>
+T ceil(T a, T b) {
+	return (a + b - 1) / b;
+}
+
+template <typename T>
+T gcd(T a, T b) {
+	return b ? gcd(b, a % b) : a;
+}
+
+long long lcd(long long a, long long b) {
+	if (a < b) {
+		swap(a, b);
+	}
+	a /= gcd(a, b);
+	return a * b;
+}
+
+template <typename T>
+vector<T> allDivisors(T n) {
+	vector<T> divisors;
+	
+	for (int i = 1 ; i <= sqrt(n) ; ++i) {
+		if (n % i == 0) {
+			divisors.push_back(i);
+			if (n / i != i) {
+				divisors.push_back(n / i);
+			}
+		}
+	}
+	return divisors;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	long long r;
+	
+	cin >> r;
+	
+	if (r % 2 == 1 && r >= 5) {
+		cout << 1 << " " << (r - 3) / 2 << "\n";
+	}
+	else {
+		cout << "NO\n";
+	}
+	
+	return 0;
+}
