b17366e3ced169abdad2dd6fa897690a1b946571
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-09-02 20:00:24 -0500

    1300/1101B

---
+++ CP/1300/1101B.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+template <typename T>
+T ceil(T a, T b) {
+	return (a + b - 1) / b;
+}
+
+template <typename T>
+T gcd(T a, T b) {
+	return b ? gcd(b, a % b) : a;
+}
+
+long long lcd(long long a, long long b) {
+	if (a < b) {
+		swap(a, b);
+	}
+	a /= gcd(a, b);
+	return a * b;
+}
+
+template <typename T>
+vector<T> allDivisors(T n) {
+	vector<T> divisors;
+	
+	for (int i = 1 ; i <= sqrt(n) ; ++i) {
+		if (n % i == 0) {
+			divisors.push_back(i);
+			if (n / i != i) {
+				divisors.push_back(n / i);
+			}
+		}
+	}
+	return divisors;
+}
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	string str;
+	cin >> str;
+	
+	int siz = (int)str.size();
+	
+	int i = 0;
+	int j = (int)str.size() - 1;
+	bool can = false;
+	
+	for (i ; i < (int)str.size() ; ++i) {
+		if (str[i] == '[') {
+			++i;
+			can = true;
+			break;
+		}
+		--siz;
+	}
+	
+	if (!can) {
+		cout << "-1\n";
+		return 0;
+	}
+	can = false;
+	for (i ; i < (int)str.size(); ++i) {
+		if (str[i] == ':') {
+			++i;
+			can = true;
+			break;
+		}
+		--siz;
+	}
+	
+	if (!can) {
+		cout << "-1\n";
+		return 0;
+	}
+	can = false;
+	
+	for (j ; j >= i ; --j) {
+		if (str[j] == ']') {
+			--j;
+			can = true;
+			break;
+		}
+		--siz;
+	}
+	
+	if (!can) {
+		cout << "-1\n";
+		return 0;
+	}
+	can = false;
+	
+	for (j ; j >= i ; --j) {
+		if (str[j] == ':') {
+			--j;
+			can = true;
+			break;
+		}
+		--siz;
+	}
+	
+	if (!can) {
+		cout << "-1\n";
+		return 0;
+	}
+	for (i ; i <= j ; ++i) {
+		if (str[i] != '|') {
+			--siz;
+		}
+	}
+	
+	cout << siz << "\n";
+	
+	
+	return 0;
+}
