7c90a99e006e1346179b118c4e1eb1132f9aa97d
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-09-04 17:15:54 -0500

    Custom hooks on forms and useEffect

--- Web Development/React/Hooks/src/index.js
+++ Web Development/React/Hooks/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 //import { HookApp } from './HookApp';
 //import { CounterApp } from './components/01-useState/CounterApp';
-import { CounterCustomHook } from './components/01-useState/CounterCustomHook';
+//import { CounterCustomHook } from './components/01-useState/CounterCustomHook';
+//import { SimpleForm } from './components/02-useEffect/SimpleForm';
+//import { FormCustomHook } from './components/02-useEffect/FormCustomHook';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 
 ReactDOM.render (
-    <CounterCustomHook />,
+    <MultipleCustomHooks />,
     document.getElementById('root')
 );
