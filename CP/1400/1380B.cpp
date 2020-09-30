6bdd9ffa499a047496096a8035bf786855b70a65
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-09-30 18:41:33 -0500

    1400/1380B

---
+++ CP/1400/1380B.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+template <typename T>
+T ceil(T a, T b) {
+	return (a + b - 1) / b;
+}
+
+template <typename T>
+T gcd(T a, T b) {
+	return b ? gcd(b, a % b) : a;
+}
+
+long long lcd(long long a, long long b) {
+	if (a < b) {
+		swap(a, b);
+	}
+	a /= gcd(a, b);
+	return a * b;
+}	
+
+void solve() {
+	string str;
+	
+	map<char, int> mp;
+	cin >> str;
+	for (char chr : str) {
+		++mp[chr];
+	}
+	
+	char ans;
+	int nax = 0;
+	
+	
+	for (pair<char, int> pa : mp) {
+		if (pa.second > nax) {
+			nax = pa.second;
+			ans = pa.first;
+		}
+	}
+	if (ans == 'R') {
+		ans = 'P';
+	}
+	else if(ans == 'P') {
+		ans = 'S';
+	}
+	else {
+		ans = 'R';
+	}
+	
+	for (int i = 0 ; i < (int)str.size() ; ++i) {
+		cout << ans;
+	}
+	
+	
+	cout << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	
+	while(t--) {
+		solve();
+	}
+	
+	return 0;
+}
