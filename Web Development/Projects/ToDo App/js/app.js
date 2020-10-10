6ea22908f2f937b32a672d885570d7e689f652ba
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-10-10 16:38:26 -0500

    reorganize code with classes

--- Web Development/Projects/ToDo App/js/app.js
+++ Web Development/Projects/ToDo App/js/app.js
@@ -1,45 +1,121 @@
+const inputText = document.getElementById('new-todo-text');
+const btn = document.getElementById('btn-add');
+
 class UI {
 
-}
+    printTodos({ todos }) {
 
-const btnAdd = document.getElementById('btn-add');
+        console.log("Desde aca");
+        console.log(todos);
 
-const eventListeners = () => {
-    btnAdd.addEventListener('click', addTodo);
+        this.cleanTodos();
 
-}
+        todos.forEach(todo => {
+            const { id, text } = todo;
 
-const addTodo = () => {
-    const newTodo = document.getElementById('new-todo-text');
+            const newLI = document.createElement('li');
+            const p = document.createElement('p');
+            const editBtn = document.createElement('button');
+            const delBtn = document.createElement('button');
+            const btnDiv = document.createElement('div');
+            
+            newLI.className = 'todo-item';
+            newLI.id = id;
+            p.textContent = text;
+            btnDiv.className = 'bttns';
 
-    if (newTodo.value.length > 0) {
-        const newLI = document.createElement('li');
-        const p = document.createElement('p');
-        const editBtn = document.createElement('button');
-        const delBtn = document.createElement('button');
-        const id = Date.now();
-    
-        newLI.className = 'todo-item';
-        newLI.id = id;
+            editBtn.classList.add('btn', 'edit-del-btn');
+            delBtn.classList.add('btn', 'edit-del-btn');
+            editBtn.innerHTML = `<i class="fas fa-edit"></i>`;
+            delBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
 
-        p.textContent = newTodo.value;
-        editBtn.classList.add('btn', 'edit-del-btn');
-        delBtn.classList.add('btn', 'edit-del-btn');
+            btnDiv.appendChild(editBtn);
+            btnDiv.appendChild(delBtn);
+            
+            newLI.appendChild(p);
+            newLI.appendChild(btnDiv);
 
-        editBtn.innerHTML = `<i class="fas fa-edit"></i>`;
-        delBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+            delBtn.onclick = () => deleteTodo(id);
+            editBtn.onclick = () => editTodo(id);
 
-        newLI.appendChild(p);
-        newLI.appendChild(editBtn);
-        newLI.appendChild(delBtn);
-    
-        delBtn.onclick = () => deleteTodo(id);
-        editBtn.onclick = () => editTodo(id);
+            document.querySelector('.todo-list').appendChild(newLI);
 
-        document.querySelector('.todo-list').appendChild(newLI);
+            inputText.value = '';
 
-        newTodo.value = '';
+            console.log(newLI);
+        });
+    }
+
+    cleanTodos() {
+        const todoItems = document.querySelectorAll('.todo-item');
+        for (let i = 0 ; i < todoItems.length ; ++i) {
+            todoItems[i].remove();
+        }
     }
 }
 
+class Todos {
+    constructor() {
+        this.isEditing = false;
+        this.todos = [];
+    }
+
+    addTodo(todo) {
+        this.todos = [...this.todos, todo];
+        console.log(todos);
+    }
+
+}
+
+const newTodo = {
+    id: '',
+    text: ''
+};
+
+const ui = new UI();
+const todos = new Todos();
+
+const addTodo = () => {
+    const { value } = inputText;
+    const length = value.length;
+    
+    if (length > 0 && length <= 75) {
+        newTodo.id = Date.now();
+        newTodo.text = value;
+
+        todos.addTodo({ ...newTodo }); // Sending copy of newTodo
+        ui.printTodos(todos);
+    }
+}
+
+
+const eventListeners = () => {
+    btn.addEventListener('click', addTodo);
+};
+
 eventListeners();
+
+
+
+/*
+
+
+eventListeners();
+
+const deleteTodo = (id) => {
+    const todo = document.getElementById(id);
+    todo.remove();
+}
+
+const editTodo = (id) => {
+    const todo = document.getElementById(id);
+    const input = document.getElementById('new-todo-text');
+    const inputBtn = document.getElementById('btn-add');
+
+    inputBtn.textContent = 'Edit TODO';
+    input.placeholder = 'Edit yout TODO';
+    input.focus();
+
+    isEditingTodo = true;
+}
+*/
