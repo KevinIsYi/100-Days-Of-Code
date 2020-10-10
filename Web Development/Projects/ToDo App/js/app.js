e9b65cb3306ece452da99a4ec3ac8df19c3a1ffa
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-10-09 20:22:49 -0500

    Todos can be added

--- Web Development/Projects/ToDo App/js/app.js
+++ Web Development/Projects/ToDo App/js/app.js
@@ -6,21 +6,40 @@ const btnAdd = document.getElementById('btn-add');
 
 const eventListeners = () => {
     btnAdd.addEventListener('click', addTodo);
+
 }
 
 const addTodo = () => {
-    const newTodoText = document.getElementById('new-todo-text').value;
+    const newTodo = document.getElementById('new-todo-text');
 
-    const newLI = document.createElement('li');
-    newLI.className = 'todo-item';
-
-
+    if (newTodo.value.length > 0) {
+        const newLI = document.createElement('li');
+        const p = document.createElement('p');
+        const editBtn = document.createElement('button');
+        const delBtn = document.createElement('button');
+        const id = Date.now();
     
-    newLI.innerHTML = `
+        newLI.className = 'todo-item';
+        newLI.id = id;
 
+        p.textContent = newTodo.value;
+        editBtn.classList.add('btn', 'edit-del-btn');
+        delBtn.classList.add('btn', 'edit-del-btn');
+
+        editBtn.innerHTML = `<i class="fas fa-edit"></i>`;
+        delBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+
+        newLI.appendChild(p);
+        newLI.appendChild(editBtn);
+        newLI.appendChild(delBtn);
     
-    `;
+        delBtn.onclick = () => deleteTodo(id);
+        editBtn.onclick = () => editTodo(id);
 
+        document.querySelector('.todo-list').appendChild(newLI);
+
+        newTodo.value = '';
+    }
 }
 
 eventListeners();
