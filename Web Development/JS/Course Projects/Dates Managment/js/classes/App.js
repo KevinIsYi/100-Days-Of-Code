05075779333b31cab6731b088305f02e12df29d1
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-10-19 18:15:25 -0500

    Maded dates project a modular one

---
+++ Web Development/JS/Course Projects/Dates Managment/js/classes/App.js
@@ -0,0 +1,28 @@
+import { datosCita, nuevaCita } from '../functions.js';
+import {
+    mascotaInput, 
+    propietarioInput, 
+    telefonoInput, 
+    fechaInput, 
+    horaInput, 
+    sintomasInput, 
+    formulario 
+} from '../selectors.js';
+
+class App {
+    constructor() {
+        this.initApp();   
+    }
+    initApp() {        
+        mascotaInput.addEventListener('input', datosCita);
+        propietarioInput.addEventListener('input', datosCita);
+        telefonoInput.addEventListener('input', datosCita);
+        fechaInput.addEventListener('input', datosCita);
+        horaInput.addEventListener('input', datosCita);
+        sintomasInput.addEventListener('input', datosCita);
+    
+        formulario.addEventListener('submit', nuevaCita);
+    }
+}
+
+export default App;
