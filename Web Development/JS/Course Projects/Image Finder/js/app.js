0743e735d1d700deeebc9660a8766898a0264d47
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-11-20 12:08:31 -0600

    connection to pixabay api and showin results

---
+++ Web Development/JS/Course Projects/Image Finder/js/app.js
@@ -0,0 +1,76 @@
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+const validarFormulario = (e) => {
+    e.preventDefault();
+
+    const terminoBusqueda = document.querySelector('#termino').value;
+
+    if (terminoBusqueda === '') {
+        mostrarAlerta('No hay texto');
+    } 
+    else {
+        buscarImagenes(terminoBusqueda);
+    }
+}
+
+const mostrarAlerta = (mensaje) => {
+
+    const existeAlerta = document.querySelector('.bg-red-100');
+    if (!existeAlerta) {
+        const alerta = document.createElement('p');
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+    
+        alerta.innerHTML = `
+            <strong class="font-bold">¡Error!</strong>
+            <span class="block sm:inline">${ mensaje }</span>
+        `;
+    
+        formulario.appendChild(alerta);
+    
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
+}
+
+const buscarImagenes = (termino) => {
+    const key = '19198783-5925320eb98a8920550b8b8c8';
+    const url = `https://pixabay.com/api/?key=${ key }&q=${ termino }&per_page=100`;
+
+    fetch(url)
+        .then(resp => resp.json())
+        .then(res => {
+            mostrarImagenes(res.hits);
+        })
+}
+
+const mostrarImagenes = (imagenes) => {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${ previewURL }" >
+                    <div class="p-4">
+                        <p class="font-bold">${ likes }<span class="font-light"> Me gusta</span></p>
+                        <p class="font-bold">${ views }<span class="font-light"> Vistas</span></p>
+                        <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p1" href="${ largeImageURL }" target="_blank" rel="noopener noreferrer">
+                            Ver Imagen
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+
+
+window.onload = () => {
+    formulario.addEventListener('submit', validarFormulario);
+}
