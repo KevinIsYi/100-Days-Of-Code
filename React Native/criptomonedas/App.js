f06b229ed2ffbfa4db9737852266adfa90b30036
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-12-18 12:40:11 -0600

    React-native skeleton and header component

---
+++ React Native/criptomonedas/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+
+import { Header } from './components/Header';
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+
+export default App;
