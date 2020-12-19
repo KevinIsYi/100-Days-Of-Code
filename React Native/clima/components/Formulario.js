e80af2166572b03229b26dbeaadd3b9603c958cd
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-12-18 18:47:23 -0600

    react-native skeleton and animation on button

---
+++ React Native/clima/components/Formulario.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+
+import { Text, View, TextInput, StyleSheet, TouchableWithoutFeedback, Animated } from 'react-native';
+import { Picker } from '@react-native-picker/picker'
+
+export const Formulario = () => {
+
+    const [ animacionBoton ] = useState(new Animated.Value(1));
+
+    const animacionEntrada = () => {
+        Animated.spring(animacionBoton, {
+            toValue: .9    
+        }).start();
+    }
+    const animacionSalida = () => {
+        Animated.spring(animacionBoton, {
+            toValue: 1,
+            friction: 4, // brincos
+            tension: 30 // menor, mas suaves el movimiento
+        }).start();
+    }
+
+    const estiloAnimacion = {
+        transform: [{
+            scale: animacionBoton
+        }]
+    }
+
+    return (
+        <>
+            <View>
+                <View style={ styles.formulario }>
+                    <TextInput
+                        style={ styles.input }
+                        placeholder="Ciudad"
+                        placeholderTextColor="#666"
+                    />
+                </View>
+                <View>
+                    <Picker
+                        itemStyle={{ height: 120, backgroundColor: '#FFF' }}
+                    >
+                        <Picker.Item label="-- Seleccione un país --" value="" />
+                        <Picker.Item label="Estados Unidos" value="US" />
+                        <Picker.Item label="México" value="MX" />
+                        <Picker.Item label="Argentina" value="AR" />
+                        <Picker.Item label="Colombia" value="CO" />
+                        <Picker.Item label="Costa Rica" value="CR" />
+                        <Picker.Item label="España" value="ES" />
+                        <Picker.Item label="Perú" value="PE" />
+                    </Picker>
+                </View>
+                <TouchableWithoutFeedback
+                    onPressIn={ animacionEntrada }
+                    onPressOut={ animacionSalida }
+                >
+                    <Animated.View
+                        style={ [styles.btnBuscar, estiloAnimacion] }
+                    >
+                        <Text style={ styles.textoBuscar }>Buscar Clima</Text>
+                    </Animated.View>
+                </TouchableWithoutFeedback>
+            </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+        padding: 10,
+        height: 50,
+        backgroundColor: '#FFF',
+        fontSize: 20,
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    btnBuscar: {
+        marginTop: 50,
+        backgroundColor: '#000',
+        padding: 10,
+        justifyContent: 'center'
+    },
+    textoBuscar: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        textAlign: 'center',
+        fontSize: 18
+    }
+});
