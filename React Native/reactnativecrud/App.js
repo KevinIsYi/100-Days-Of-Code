675c8202307900c3c0013edd6a51ba176afce41b
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-12-26 13:40:07 -0600

    Install react-navigation/first commit

---
+++ React Native/reactnativecrud/App.js
@@ -0,0 +1,20 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>Hola</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
