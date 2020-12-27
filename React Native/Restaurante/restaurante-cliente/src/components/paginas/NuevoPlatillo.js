6f384ca034d58eafbf0390f39cdbecf5ce77e056
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-12-27 15:44:48 -0600

    Finished with form component

--- React Native/Restaurante/restaurante-cliente/src/components/paginas/NuevoPlatillo.js
+++ React Native/Restaurante/restaurante-cliente/src/components/paginas/NuevoPlatillo.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const NuevoPlatillo = () => {
+
+    const [ formValues, setFormValues ] = useState({
+        nombre: '',
+        precio: '',
+        categorias: '',
+        imagen: '',
+        descripcion: ''
+    });
+
+    const { nombre, precio, categorias, imagen, descripcion } = formValues;
+
+    const handleFormInputChange = (e) => {
+        const { target:{ name, value } } = e;
+        setFormValues({
+            ...formValues,
+            [name]: value
+        });
+    };
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+    };
+
     return (
         <>
             <h1 className="text-3xl font-light mb-4">Agregar Platillo</h1>
             <div className="flex justify-center mt-10">
                 <div className="w-full max-w-3xl">
-                    <form>
+                    <form
+                        onSubmit={ handleSubmit }
+                    >
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bolg mb-2" htmlFor="nombre">Nombre</label>
                             <input 
@@ -14,6 +42,9 @@ export const NuevoPlatillo = () => {
                                 id="nombre"
                                 type="text"
                                 placeholder="Nombre del Platillo"
+                                name="nombre"
+                                value={ nombre }
+                                onChange={ handleFormInputChange }
                             />
                         </div>
                         <div className="mb-4">
@@ -23,7 +54,10 @@ export const NuevoPlatillo = () => {
                                 id="precio"
                                 type="number"
                                 placeholder="$20"
+                                name="precio"
                                 min="0"
+                                value={ precio }
+                                onChange={ handleFormInputChange }
                             />
                         </div>
                         <div className="mb-4">
@@ -31,7 +65,9 @@ export const NuevoPlatillo = () => {
                             <select
                                 className="shadow appearance-nonde border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="precio"
-                                name="categoria"
+                                name="categorias"
+                                value={ categorias }
+                                onChange={ handleFormInputChange }
                             >
                                 <option value="">-- Seleccione --</option>
                                 <option value="desayuno">Desayuno</option>
@@ -48,6 +84,9 @@ export const NuevoPlatillo = () => {
                                 className="shadow appearance-nonde border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="imagen"
                                 type="file"
+                                name="imagen"
+                                value={ imagen }
+                                onChange={ handleFormInputChange }
                             />
                         </div>
                         <div className="mb-4">
@@ -56,12 +95,16 @@ export const NuevoPlatillo = () => {
                                 className="shadow appearance-nonde border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-40"
                                 id="descripcion"
                                 placeholder="Descripción del Platillo"
+                                name="descripcion"
+                                value={ descripcion }
+                                onChange={ handleFormInputChange }
                             ></textarea>
                         </div>
                         <input
                             className="bg-gray-800 hover:bg-gray-900 w-full mt-5 p-2 text-white uppercase font-bold"
                             type="submit"
                             value="Agregar Platilo"
+                            readOnly={ true }
                         />
                     </form>
                 </div>
