eba94483b0e412aeb0b2d60ae8d5bb2ffe19b0f7
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-12-27 14:45:44 -0600

    Added routing and finished with the css of the components

--- React Native/Restaurante/restaurante-cliente/src/App.js
+++ React Native/Restaurante/restaurante-cliente/src/App.js
@@ -1,13 +1,21 @@
 import { Switch, Route } from 'react-router-dom';
+import { Menu } from './components/paginas/Menu';
+import { NuevoPlatillo } from './components/paginas/NuevoPlatillo';
 import { Ordenes } from './components/paginas/Ordenes';
+import { Sidebar } from './components/ui/Sidebar';
 
 function App() {
   return (
-    <>
-      <Switch>
-        <Route path="/" component={ Ordenes } />
-      </Switch>
-    </>
+    <div className="md:flex min-h-screen">
+    <Sidebar />
+      <div className="md:w-3/5 xl:w-4/5 p-6">
+        <Switch>
+          <Route exact path="/" component={ Ordenes } />
+          <Route exact path="/menu" component={ Menu } />
+          <Route exact path="/nuevo-platillo" component={ NuevoPlatillo } />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
--- React Native/Restaurante/restaurante-cliente/src/components/paginas/Menu.js
+++ React Native/Restaurante/restaurante-cliente/src/components/paginas/Menu.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Menu = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <h1 className="text-3xl font-light mb-4">Menú</h1>
+            <Link to="nuevo-platillo" className="bg-blue-800 hover:bg-blue-700 inline-block mb-5 p-2 text-white uppercase font-bold">Agregar Platillo</Link>
+        </>
     )
 }
