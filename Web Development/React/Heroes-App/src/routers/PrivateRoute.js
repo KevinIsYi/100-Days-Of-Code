0b76eefcb34f6b790f3bf65642095881fd44e897
Kevin <rodriguezkevin95g@gmail.com>
2020-12-28 16:39:04 -0600

    Added routes protection

---
+++ Web Development/React/Heroes-App/src/routers/PrivateRoute.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export const PrivateRoute = ({ 
+    isAuthenticated,
+    component: Component,
+    ...rest
+}) => {
+
+    localStorage.setItem('lastPath', rest.location.pathname);
+    //En rest.location.search se guarda el query
+    return (
+        <Route
+            { ...rest } 
+                component={
+                    (props) => (
+                        (isAuthenticated
+                            ? <Component { ...props } />
+                            : (<Redirect to="/login" />)
+                        )    
+                    )
+            }   
+        />    
+    )
+}
+
+PrivateRoute.propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    component: PropTypes.func.isRequired
+}
