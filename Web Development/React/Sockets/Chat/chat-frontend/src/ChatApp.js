5c8724e3f4d58a5a0a41b8ee92609b4a3c6b7c5d
Kevin <rodriguezkevin95g@gmail.com>
2021-02-02 12:06:36 -0600

    Added routing and register/logins creens

--- Web Development/React/Sockets/Chat/chat-frontend/src/ChatApp.js
+++ Web Development/React/Sockets/Chat/chat-frontend/src/ChatApp.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const ChatApp = () => {
     return (
-        <div>
-            ChatApp
-        </div>
+        <AppRouter />
     )
 }
---
+++ Web Development/React/Sockets/Chat/chat-frontend/src/router/AppRouter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from 'react-router-dom';
+import { ChatPage } from '../pages/ChatPage';
+import { AuthRouter } from './AuthRouter';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <>
+                <Switch>
+                    <Route path="/auth" component={AuthRouter} />
+                    <Route exact path="/" component={ChatPage} />
+                    
+                    <Redirect to="/" />
+                </Switch>
+            </>
+        </Router>
+    )
+}
