35bbb71b39cf566c176dc1408c0d2954cabc9454
Kevin <rodriguezkevin95g@gmail.com>
2021-02-11 18:37:40 -0600

    Basics of TS

---
+++ Web Development/TS/app.ts
@@ -0,0 +1,12 @@
+// Types
+const number: number = 1e30;
+const string: string = '';
+const boolean: boolean = true;
+
+console.log(number);
+
+const add = (x: number, y: number): number => {
+    return x + y;
+}
+
+console.log(add(10, 20)); //returns 30
